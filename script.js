11252a933ba35e0938f4bbbe1dc6ba4bb52807e1
aryadot <63002908+aryadot@users.noreply.github.com>
2025-06-28 09:34:23 -0400

    vi

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ document.addEventListener('DOMContentLoaded', () => {
   startButton.addEventListener('click', () => {
     startScreen.style.display = 'none';
     videoOverlay.style.display = 'block';
+    introVideo.load(); // Reload to avoid stuck playback
     introVideo.play();
   });
 
